2f97858606e6e6bf7cce06380e25be649aef14d7
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2024-04-20 12:42:56 +0200

    Update migration

--- InfoCenter.Api/Data/Migrations/20240420093602_UpdateDeleteBehavior.Designer.cs
+++ InfoCenter.Api/Data/Migrations/20240420093602_UpdateDeleteBehavior.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using InfoCenter.Api;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace InfoCenter.Api.Data.Migrations
 {
     [DbContext(typeof(InfoCenterContext))]
-    partial class InfoCenterContextModelSnapshot : ModelSnapshot
+    [Migration("20240420093602_UpdateDeleteBehavior")]
+    partial class UpdateDeleteBehavior
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.4");
